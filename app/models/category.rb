49fecc00f3948f67b2b733fef72f7b9432372a90
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-02 18:58:53 +0300

    Create Category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :transactions, class_name: 'Transaction', foreign_key: 'category_id'
 end
