b7094f58178fe0a4235966854c98ba02ce11cc17
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-02 19:56:07 +0300

    Create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/update'
+  get 'categories/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
