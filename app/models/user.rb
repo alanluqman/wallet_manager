9ad00d530c99a7cc01486866fe635c0bb42f8e20
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-02 23:00:59 +0300

    Add validation on to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :categories, class_name: 'Category', foreign_key: 'author_id'
   has_many :transactions, class_name: 'Transaction', foreign_key: 'author_id'
+
+  validates :name, presence: true
 end
