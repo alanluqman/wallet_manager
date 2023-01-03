89eaacf36492316e98a1a2436651268ddfe97897
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-03 17:30:31 +0300

    Update Transaction validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :category, class_name: 'Category'
 
-  validates :name, :amount, presense: true
+  validates :name, :amount, presense: { strict: true }
 end
