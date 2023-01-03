f8e36ff798de2b5223a16777036ef87beaf0e820
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-03 17:28:27 +0300

    Update User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :categories, class_name: 'Category', foreign_key: 'author_id'
   has_many :transactions, class_name: 'Transaction', foreign_key: 'author_id'
 
-  validates :name, presence: true
+  validates :name, :email, presence: { strict: true }
 end
