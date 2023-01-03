587e671a35ef5e0f6b0c6debbdb22283507b2fc7
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-03 16:49:19 +0300

    Add authentication on user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
-  get 'transactions/index'
-  get 'transactions/show'
-  get 'transactions/new'
-  get 'transactions/create'
-  get 'transactions/update'
-  get 'transactions/destroy'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/update'
-  get 'categories/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "categories#index"
+  resources :users do
+    resources :categories do
+      resources :transactions
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
