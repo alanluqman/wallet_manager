39dafb5d4ff96ab287d8fc6b8404f2be4f232d6b
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-05 21:52:42 +0300

    Implement categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,33 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = current_user.categories
+  end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @tracsactions = Transaction.where(category_id: params[:id]).order(created_at: :desc)
+  end
 
   def new; end
 
-  def create; end
+  def create
+    new_category = current_user.categories.new(category_params)
+    if new_category.save
+      redirect_to user_categories_path(user_id: current_user.id)
+    else
+      flash.alert = "Opps, Category does not created !"
+    end
+  end
 
-  def update; end
+  def update
+  
+  end
 
   def destroy; end
+
+  private
+
+  def category_params
+    params.permit(:name, :icon)
+  end
 end
