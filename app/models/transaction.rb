fc10dc338f531a5052d6e2202f8acf147545a4d4
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-06 14:15:53 +0300

    Update validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :category, class_name: 'Category'
 
-  validates :name, :amount, presence: { strict: true }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true
 end
